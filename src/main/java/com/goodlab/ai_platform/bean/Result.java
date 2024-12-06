5ddf66e3df4738d6a28d1b412712a4e726bd4635
nidielaila1 <3294200@qq.com>
2024-12-06 20:22:14 +0800

    new mod

---
+++ src/main/java/com/goodlab/ai_platform/bean/Result.java
@@ -0,0 +1,17 @@
+package com.goodlab.ai_platform.bean;
+
+
+import lombok.Generated;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@Generated
+public class Result {
+
+    private String id;
+    private String batch;
+    private String graph;
+}
