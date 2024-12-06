b4d1d9f1c7707b7335dd58ca4c7e5c624903ecfe
nidielaila1 <3294200@qq.com>
2024-12-06 15:05:03 +0800

    new module

---
+++ src/main/java/com/goodlab/ai_platform/AiPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.goodlab.ai_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiPlatformApplication.class, args);
+	}
+
+}
